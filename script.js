9b45be7f15a3ee5a30373e9a571364a7cd6b43a6
Douglas-Santana <douguinhodns@gmail.com>
2021-11-03 18:01:38 -0300

    implementação do botao apagar tudo, remover finalizados, salvar tarefas e remover selecionado

--- script.js
+++ script.js
@@ -1,41 +1,79 @@
-let input = document.querySelector('#texto-tarefa');
-let button = document.querySelector('#criar-tarefa');
-let list = document.querySelector('#lista-tarefas');
+const listTask = document.querySelector('#lista-tarefas');
 
-button.addEventListener('click', click);
+// Adiciona a tarefa na lista
+const inputTask = document.querySelector('#texto-tarefa');
+const btnAddTask = document.querySelector('#criar-tarefa');
 
-function click() {
-    let listLi = document.createElement('li');
-    listLi.innerText = input.value;
-    list.appendChild(listLi);
-    input.value = '';
-}
+btnAddTask.addEventListener('click', () => {
+    const listLi = document.createElement('li');
+    listLi.innerText = inputTask.value;
+    listTask.appendChild(listLi);
+    inputTask.value = '';
+});
 
-//requisito 7 e 8
- list.addEventListener('click', function clickItem(e) {
-    let listItem = list.childNodes;
-    for (let index = 0; index < listItem.length; index += 1) {
-        listItem[index].style.backgroundColor = 'white';
+// Ao clicar no item recebe uma cor de fundo
+const listItems = listTask.childNodes;
+ listTask.addEventListener('click', (e) => {
+    for (let index = 0; index < listItems.length; index += 1) {
+        listItems[index].style.backgroundColor = 'rgba(133, 133, 48, 0)';
     }
     if (e.target.localName === 'li')
     e.target.style.backgroundColor = 'rgb(128, 128, 128)';
 });
 
-//requisito 9
-list.addEventListener('dblclick', function clickItem(e) {
-    //let listItem = list.childNodes;
-    //for (let index2 = 0; index2 < listItem.length; index2 += 1) {
-        //listItem[index2].classList.remove('completed');
-        //listItem[index2].style.textDecoration = 'none';
-    //}
-    //e.target.style.textDecoration = 'line-through solid rgb(0, 0, 0)';
+// Excluir item
+const btnDeleteSelected = document.querySelector('#excluir-item');
+btnDeleteSelected.addEventListener('click', deleteSelected);
+function deleteSelected() {
+    for (let index = 0; index < listItems.length; index += 1) {
+        if (listItems[index].style.backgroundColor === 'rgb(128, 128, 128)') {
+            listItems[index].remove()
+        }
+    }
+}
+
+// Ao clica duas vezes no item ele é marcado
+listTask.addEventListener('dblclick', (e) => {
     if (e.target.localName === 'li')
     e.target.classList.toggle('completed');
 });
-/* list.addEventListener('dblclick', function clickItem(e) {
-    let listItem = list.childNodes;
-    for (let index3 = 0; index3 < listItem.length; index3 += 1) {
-        listItem[index3].classList.remove('completed');
-        listItem[index3].style.textDecoration = 'none';
+
+// Remove apenas os itens marcados
+const btnRemoveCompleted = document.querySelector('#remover-finalizados');
+btnRemoveCompleted.addEventListener('click', () => {
+    document.querySelectorAll('.completed').forEach((item) => item.remove())
+})
+
+// Remove todos os itens da lista
+const btnClearAllTasks = document.querySelector('#apaga-tudo');
+btnClearAllTasks.addEventListener('click', () => {
+    listTask.innerHTML = '';
+    localStorage.clear();
+})
+
+// Salva a lista no local storage
+const btnSalveList = document.querySelector('#salvar-tarefas');
+btnSalveList.addEventListener('click', () => {
+    let itemSalved = [listItems[0].textContent]
+    for (let index = 1; index < listItems.length; index ++) {
+        
+        itemSalved.push(listItems[index].textContent)
     }
-}); */
+    localStorage.setItem('itemsSaved', itemSalved)
+})
+
+// Permanecer itens salvos depois de recarregar a pagina
+function loadListSaved() {
+    if (localStorage.length > 0) {
+        const listSaved = localStorage.getItem('itemsSaved').split(',')
+        for (let index = 0; index < listSaved.length; index ++) {
+            const listLi = document.createElement('li');
+            listTask.appendChild(listLi);
+            listLi.innerText = listSaved[index]
+        }
+    }
+}
+
+window.onload = () => {
+    loadListSaved();
+}
